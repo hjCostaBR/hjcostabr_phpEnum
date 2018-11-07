d19886b8e48f1af91a03b9633f4e2e3dd469157f
Hebert Costa <hjcosta@outlook.com.br>
2018-11-06 23:38:32 -0200

    Update EnumException.php
    comentarios & indentacao

--- src/EnumException.php
+++ src/EnumException.php
@@ -6,13 +6,13 @@ namespace hjcostabr\phpEnum;
  * Exception to be thrown in case of enum package related error.
  * 
  * @namespace hjcostabr\phpEnum
- * @author hjCostaBR
+ * @author hjcostabr
  */
 class EnumException extends \Exception
 {
     const INVALID_ENUM_ELEMENT_PROPERTY = 1;
-    const CODE_NOT_DEFINED              = 2;
-    const ELEMENT_NOT_FOUND             = 3;
+    const CODE_NOT_DEFINED = 2;
+    const ELEMENT_NOT_FOUND = 3;
 
     /**
      * EnumException constructor.
